f9fee91a5da0e2698039ddd020e248a5d0b91752
sabrina <sabrinadjebbar93@gmail.com>
2022-03-14 21:51:11 -0400

    Completed assignmemt

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat(name){
+     return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+     return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift();
+}
+
+function appendCat(name){
+ return [...cats, name];
+   
+}
+
+function prependCat(name){
+    return [name, ...cats ];
+}
+
+function removeLastCat(name){
+    return cats.slice(0, cats.length -1);
+}
+
+function removeFirstCat(name){
+    return cats.slice(1);
+}
+
+
